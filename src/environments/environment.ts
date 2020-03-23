9f0aad6077e89607b392296176990fada030b77c
Vladimir Romaniuk <romaniukvr@yandex.ru>
2020-03-23 17:53:38 +0300

    Added firebase to modules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDToBvhS-o1461bDHDKCX8ZgG-CtZRGz0s',
+    authDomain: 'share-your-drive-8c7b1.firebaseapp.com',
+    databaseURL: 'https://share-your-drive-8c7b1.firebaseio.com',
+    projectId: 'share-your-drive-8c7b1',
+    storageBucket: 'share-your-drive-8c7b1.appspot.com',
+    messagingSenderId: '745110505398',
+    appId: '1:745110505398:web:7db0f9b5cfdb925857e317',
+    measurementId: 'G-NKY87R77RG'
+  },
 };
 
 /*
