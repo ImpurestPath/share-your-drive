844ce6ff79006048285a82fd737587f38beb7edb
Jarkko Kuha <50085923+Kuhaija@users.noreply.github.com>
2020-04-26 22:56:04 +0300

    Someone forgot to commit these like, ever

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { RouteReuseStrategy } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular'
-import { SplashScreen } from '@ionic-native/splash-screen/ngx'
-import { StatusBar } from '@ionic-native/status-bar/ngx'
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component'
-import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
-import { environment } from '../environments/environment'
+import { environment } from '../environments/environment';
 
-import { AngularFireModule } from '@angular/fire'
-import { AngularFirestoreModule } from '@angular/fire/firestore'
-import { AngularFireAuthModule } from '@angular/fire/auth'
 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { DrivesSearchPopupComponent } from './components/drives-search-popup/drives-search-popup.component'
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 import { AddFavoritePopupComponent } from './components/add-favorite-popup/add-favorite-popup.component'
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent, DrivesSearchPopupComponent, AddFavoritePopupComponent],
@@ -42,7 +42,7 @@ import { AddFavoritePopupComponent } from './components/add-favorite-popup/add-f
     NativeGeocoder,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
   ],
   bootstrap: [AppComponent]
 })
