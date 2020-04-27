deb2d9e98851bf7067f8f5876a2bb05a8e6a380e
Jarkko Kuha <50085923+Kuhaija@users.noreply.github.com>
2020-04-27 22:39:51 +0300

    fixed some merge-caused problems

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -5,10 +5,12 @@ import { DrivesActionComponent } from 'src/app/components/drives-action/drives-a
 import { IonicModule } from '@ionic/angular';
 import { AddDrivePageModule } from 'src/app/pages/add-drive/add-drive.module';
 import { Component } from '@angular/core';
+import { MyDrivesPageModule } from './pages/my-drives/my-drives.module';
+
 
 @NgModule({
- imports:      [ CommonModule,AddDrivePageModule,IonicModule],
- declarations: [ DrivesActionComponent ],
- exports:      [ DrivesActionComponent,AddDrivePageModule,CommonModule,IonicModule,FormsModule ]
+    imports: [CommonModule, AddDrivePageModule, IonicModule, MyDrivesPageModule],
+    declarations: [DrivesActionComponent],
+    exports: [DrivesActionComponent, AddDrivePageModule, CommonModule, IonicModule, FormsModule]
 })
 export class SharedModule { }
